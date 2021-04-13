6493de4a093604ad8728c34c4c335e6b10317d63
Oscar Julián Tinjacá Reyes <Ojtinjacar@unal.edu.co>
2021-04-13 15:19:59 -0500

    Update port 8300

--- proyecto/models/request_model.py
+++ proyecto/models/request_model.py
@@ -2,7 +2,7 @@ from django.db import models
 from .service_model import Service
 class Request(models.Model):
     request_id = models.AutoField(primary_key=True)
-    user_id = models.IntegerField()
+    user_id = models.CharField(max_length=30)
     date = models.DateField()
     time = models.TimeField()
     service_id = models.ForeignKey(Service, on_delete=models.CASCADE,blank=True, null=True)
