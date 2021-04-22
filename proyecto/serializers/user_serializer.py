5e1628d403420899ccb1eb536cd8a348819a0ee6
Julian <ojtinjacar@unal.edu.co>
2021-04-22 00:44:54 -0500

    delete

--- proyecto/serializers/user_serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from proyecto.models.user_model import User
-
-class UserSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = User
-        fields = ['user_id','name','doc','phone','university_id','mail','password','rh','picture','registry_date']
